9d9ec407aadaedf680e4c0c78802b90fddacf562
Methyx <methyx95@gmail.com>
2023-06-02 15:39:56 +0200

    ready to prod

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ document.querySelector("#app").innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Christel !</h1>
+    <h1>Hello</h1>
+    <h1>Ready to Prod !</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
